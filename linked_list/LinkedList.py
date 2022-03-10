92769c080840b65d4af51a12d247f2f42b171d0f
dkkishor <d.krishnakishore@gmail.com>
2022-03-10 17:32:19 -0500

    Update LinkedList.py

--- linked_list/LinkedList.py
+++ linked_list/LinkedList.py
@@ -11,32 +11,40 @@ import Node from Node
 class LinkedList:
   def __init__(self):
     self._head = None
+    self._len = 0
     
   def get_head(self):
     return self._head
   
+  def set_head(self, node):
+    self._head = node
+
   def is_empty(self):
-    return [True if self._head is None else False]
+    # return True if self._len == 0 else False
+    return True if self.get_head() is None else False
   
-  def insert_at_head(self, data):
-    newNode = Node(Data)
-    newNode.set_next_node(self._head)
-    self._head = newNode
-      
-  def insert_at_tail(self, data):
+  def get_length(self):
+    return self._len
+  
+  def incr_length(self):
+    self._len += 1
+    
+  def insert_node(self, data, pos):
     newNode = Node(data)
 
-    if (is_empty()):
-      self._head = newNode
-    else:
-      current = self._head
-      next = self._head.get_next_node()
-      
-      while next is not None:
-        current = next
-        next = next.get_next_node()
-      
-      current.next = node
-      
-        
-      
+    if self.is_empty() or pos == 1:
+      newNode.set_next_node(self.get_head())
+      self.set_head(newNode)
+      self.incr_length()
+      return
+    
+    if pos > self.get_length():
+      pos = self.get_length()
+    
+    current_node = self.get_head()
+    for i in range(pos-2):
+      current_node = current_node.get_next_node()
+
+    newNode.set_next_node(current_node.get_next_node())
+    current_node.set_next_node(newNode)
+    self.incr_length()     
