4e1e59152205eacb9189d4171f7e5ef83737e1be
dkkishor <d.krishnakishore@gmail.com>
2022-03-09 23:02:10 -0500

    Head & Tail node implementation

--- linked_list/LinkedList.py
+++ linked_list/LinkedList.py
@@ -10,27 +10,27 @@ import Node from Node
 
 class LinkedList:
   def __init__(self):
-    self.head = None
+    self._head = None
     
   def get_head(self):
-    return self.head
+    return self._head
   
   def is_empty(self):
-    return [True if self.head is None else False]
+    return [True if self._head is None else False]
   
-  def insert_at_head(self, node):
-    if (is_empty()):
-      self.head = Node
-    else:
-      node.next = self.head
-      self.head = node
+  def insert_at_head(self, data):
+    newNode = Node(Data)
+    newNode.set_next_node(self._head)
+    self._head = newNode
       
-  def insert_at_tail(self, node):
+  def insert_at_tail(self, data):
+    newNode = Node(data)
+
     if (is_empty()):
-      self.head = node
+      self._head = newNode
     else:
-      current = self.head
-      next = self.head.get_next_node()
+      current = self._head
+      next = self._head.get_next_node()
       
       while next is not None:
         current = next
