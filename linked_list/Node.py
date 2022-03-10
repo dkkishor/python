bfcac3ce87d2c5e77571929c927d538dff4b703b
dkkishor <d.krishnakishore@gmail.com>
2022-03-09 22:09:52 -0500

    Create Node.py

---
+++ linked_list/Node.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+
+'''
+- Author : Kishore D
+- Info   : Class definition of a Node in a Linked List
+            * python
+'''
+
+class Node:
+  def __init__(self, data):
+    self.data = data
+    self.next = None # pointer to the next Node
